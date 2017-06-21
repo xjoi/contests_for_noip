0fb4253b220fbc2a1d67231707a2d1760eb07365
langsike <997527270@qq.com>
2017-06-21 18:33:53 +0800

    Update A.cpp

--- langsike/contest8/A.cpp
+++ langsike/contest8/A.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 int a,b;
 int main() {
-	scanf("%d%d",&a,&b);
-	int ans=a;
-	while (a>=b) {
-		ans+=a/b;
-		a=(a%b)+a/b;
-	}
-	printf("%d\n",ans);
-	return 0;
+    scanf("%d%d",&a,&b);
+    int ans=a;
+    while (a>=b) {
+        ans+=a/b;
+        a=(a%b)+a/b;
+    }
+    printf("%d\n",ans);
+    return 0;
 }
